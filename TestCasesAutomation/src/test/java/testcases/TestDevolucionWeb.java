9b321cb9bfe086d6e9c1fa0e7f9621cdcdf8f5de
edsoncorvo7 <edsoncorvo7@gmail.com>
2022-06-23 09:51:50 -0400

    Versión 1.3

--- TestCasesAutomation/src/test/java/testcases/TestDevolucionWeb.java
+++ TestCasesAutomation/src/test/java/testcases/TestDevolucionWeb.java
@@ -20,87 +20,143 @@ import time.Time;
 
 @TestMethodOrder(OrderAnnotation.class)
 public class TestDevolucionWeb {
-private WebDriver driver;
-Time wait = new Time();
-	
+	private WebDriver driver;
+	Time wait = new Time();
+
 	@Before
-	public void setUp( ) {
-		
+	public void setUp() {
+
 		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://devo.cert.jetsm.art/cl/es/devoluciones");
-		//driver.get("https://www.google.cl/");
+		// driver.get("https://www.google.cl/");
 	}
-	
-	//@Ignore
+
+	@Ignore
 	@Test
 	@Order(1)
 	public void testPlaceholderPNREmailChile() {
 		wait.Tiempo(3000);
 		WebElement pnrEmail = driver.findElement(By.name("PnrEmail"));
 		String placehd = pnrEmail.getAttribute("placeholder");
-		
-		//Se verifica el texto dentro del textbox PnrEmail
-		assertEquals("Apellido Pasajero o Email del Comprador",placehd);
-		
+
+		// Se verifica el texto dentro del textbox PnrEmail
+		assertEquals("Apellido Pasajero o Email del Comprador", placehd);
 	}
-	
-	//@Ignore
+
+	@Ignore
 	@Test
 	@Order(2)
 	public void testPlaceholderPNREmailEnglish() {
 		wait.Tiempo(4000);
-		WebElement culturaChile = driver.findElement(By.cssSelector("#dropdown01"));
+		WebElement culturaChile = driver.findElement (By.cssSelector("#dropdown01"));
 		WebElement culturaEnglish = driver.findElement(By.xpath("//*[@id=\"dd\"]/div[4]/a"));
-		
-		culturaChile.click(); 
+
+		culturaChile.click();
 		wait.Tiempo(4000);
 		culturaEnglish.click();
 		wait.Tiempo(5000);
-		
+
 		WebElement pnrEmail = driver.findElement(By.name("PnrEmail"));
 		String placehd = pnrEmail.getAttribute("placeholder");
-		
-		//Se verifica el texto dentro del textbox PnrEmail
-		assertEquals("Passenger last name or buyer email",placehd);
-		
+
+		// Se verifica el texto dentro del textbox PnrEmail
+		assertEquals("Passenger last name or buyer email", placehd);
 	}
-	
+
+	@Ignore
 	@Test
 	@Order(3)
 	public void testPlaceholderPNREmailPeru() {
 		wait.Tiempo(4000);
-		WebElement culturaChile = driver.findElement(By.cssSelector("#dropdown01"));
+		WebElement culturaChile = driver.findElement (By.cssSelector("#dropdown01"));
 		WebElement culturaPeru = driver.findElement(By.xpath("//*[@id=\"dd\"]/div[3]/a"));
-		
+
 		culturaChile.click();
 		wait.Tiempo(4000);
 		culturaPeru.click();
 		wait.Tiempo(5000);
-		
+
 		WebElement pnrEmail = driver.findElement(By.name("PnrEmail"));
 		String placehd = pnrEmail.getAttribute("placeholder");
-		
-		//Se verifica el texto dentro del textbox PnrEmail
-		assertEquals("Apellido Pasajero o Email del Comprador",placehd);
-		
+
+		// Se verifica el texto dentro del textbox PnrEmail
+		assertEquals("Apellido Pasajero o Email del Comprador", placehd);
 	}
-	
-	/*
+
+	@Ignore
 	@Test
-	public void testGooglePage() {
-		WebElement searchbox = driver.findElement(By.name("q"));
-		
-		searchbox.clear();
-		searchbox.sendKeys("QA");
-		searchbox.submit(); // Enter
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		assertEquals("QA - Buscar con Google",driver.getTitle());
-		
+	@Order(4)
+	public void testPlaceholderPNREmailArgentina() {
+		wait.Tiempo(4000);
+		WebElement culturaChile = driver.findElement (By.cssSelector("#dropdown01"));
+		WebElement culturaArgentina = driver.findElement(By.xpath("//*[@id=\"dd\"]/div[2]/a"));
+
+		culturaChile.click();
+		wait.Tiempo(4000);
+		culturaArgentina.click();
+		wait.Tiempo(5000);
+ 
+		WebElement pnrEmail = driver.findElement(By.name("PnrEmail"));
+		String placehd = pnrEmail.getAttribute("placeholder");
+
+		// Se verifica el texto dentro del textbox PnrEmail
+		assertEquals("Apellido Pasajero o Email del Comprador", placehd);
 	}
-	*/
+
+	@Ignore
+	@Test
+	@Order(5)
+	public void testPlaceholderPNREmailColombia() {
+		wait.Tiempo(4000);
+		
+		WebElement culturaChile = driver.findElement (By.cssSelector("#dropdown01"));
+		WebElement culturaColombia = driver.findElement(By.xpath("//*[@id=\"dd\"]/div[6]/a"));
+
+		culturaChile.click();
+		wait.Tiempo(4000);
+		culturaColombia.click();
+		wait.Tiempo(5000);
+
+		WebElement pnrEmail = driver.findElement(By.name("PnrEmail"));
+		String placehd = pnrEmail.getAttribute("placeholder");
+
+		// Se verifica el texto dentro del textbox PnrEmail
+		assertEquals("Apellido Pasajero o Email del Comprador", placehd);
+	}
 	
+	@Test
+	@Order(5)
+	public void testPlaceholderPNREmailBrasil() {
+		wait.Tiempo(4000);
+		
+		WebElement culturaChile = driver.findElement (By.cssSelector("#dropdown01"));
+		WebElement culturaBrasil = driver.findElement(By.xpath("//*[@id=\"dd\"]/div[5]/a"));
+
+		culturaChile.click();
+		wait.Tiempo(4000);
+		culturaBrasil.click();
+		wait.Tiempo(5000);
+
+		WebElement pnrEmail = driver.findElement(By.name("PnrEmail"));
+		String placehd = pnrEmail.getAttribute("placeholder");
+
+		// Se verifica el texto dentro del textbox PnrEmail
+		assertEquals("Sobrenome do passageiro ou endereço de e-mail do comprador", placehd);
+	}
+
+	/*
+	 * @Test public void testGooglePage() { WebElement searchbox =
+	 * driver.findElement(By.name("q"));
+	 * 
+	 * searchbox.clear(); searchbox.sendKeys("QA"); searchbox.submit(); // Enter
+	 * driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	 * assertEquals("QA - Buscar con Google",driver.getTitle());
+	 * 
+	 * }
+	 */
+
 	@After
 	public void teardDown() {
 		driver.quit();
